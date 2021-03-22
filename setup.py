79259913a79c662e3c5452b28b4606958c03ae93
Maciej Kuraż <maciej.kuraz@gmail.com>
2021-03-22 13:02:21 +0100

    Added all files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ASK',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Maciej',
+    author_email='',
+    description='', requires=['numpy', 'pygame', 'matplotlib', 'opencv-python']
+)
